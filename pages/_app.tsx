13aa7efd246c7cf4c802732539719511e418386e
mohammedyh <mohammedyousufhamid@gmail.com>
2022-11-18 16:24:11 +0000

    configure nextjs chakra and authentication pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { theme } from '@chakra-ui/pro-theme';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import '@fontsource/inter';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}: AppProps) {
+	return (
+		<SessionProvider session={session}>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</SessionProvider>
+	);
 }
---
+++ pages/auth/login.tsx
@@ -0,0 +1,110 @@
+import {
+	Button,
+	Checkbox,
+	Container,
+	FormControl,
+	FormLabel,
+	Heading,
+	HStack,
+	Image,
+	Input,
+	Stack,
+	Text,
+	useBreakpointValue,
+} from '@chakra-ui/react';
+import { getCsrfToken, getSession, useSession } from 'next-auth/react';
+import Link from 'next/link';
+
+export default function Login({ csrfToken }: { csrfToken: string }) {
+	return (
+		<Container maxW="md" py={{ base: '12', md: '24' }}>
+			<Stack spacing="8">
+				<Stack spacing="6">
+					<Image
+						src="/bird-logo.png"
+						alt="Company."
+						width={150}
+						marginX="auto"
+					/>
+					<Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+						<Heading size={useBreakpointValue({ base: 'xs', md: 'sm' })}>
+							Log in to your account
+						</Heading>
+					</Stack>
+				</Stack>
+
+				<form method="post" action="/api/auth/callback/credentials">
+					<input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+					<Stack spacing="6">
+						<Stack spacing="5">
+							<FormControl isRequired>
+								<FormLabel htmlFor="email">Email</FormLabel>
+								<Input
+									id="email"
+									name="email"
+									placeholder="elon@tesla.com"
+									type="email"
+								/>
+							</FormControl>
+							<FormControl isRequired>
+								<FormLabel htmlFor="password">Password</FormLabel>
+								<Input
+									id="password"
+									name="password"
+									placeholder="********"
+									type="password"
+								/>
+							</FormControl>
+						</Stack>
+						<HStack justify="space-between">
+							<Checkbox defaultChecked>Remember me</Checkbox>
+							<Button variant="link" colorScheme="blue" size="sm">
+								Forgot password
+							</Button>
+						</HStack>
+						<Stack spacing="4">
+							<Button
+								variant="solid"
+								backgroundColor="black"
+								color="white"
+								_hover={{ background: 'rgba(0, 0, 0, 0.75)' }}
+								type="submit"
+							>
+								Sign In
+							</Button>
+						</Stack>
+					</Stack>
+				</form>
+
+				<HStack spacing="1" justify="center">
+					<Text fontSize="sm" color="muted">
+						Dont have an account?
+					</Text>
+
+					<Button variant="link" colorScheme="blue" size="sm">
+						<Link href="register">Sign up</Link>
+					</Button>
+				</HStack>
+			</Stack>
+		</Container>
+	);
+}
+
+// @ts-ignore
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+
+	if (session !== null) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+	return {
+		props: {
+			csrfToken: await getCsrfToken(context),
+		},
+	};
+}
