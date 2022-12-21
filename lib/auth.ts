adafd48f12656b0518283b12e9f4cd6eef7a9cb8
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-21 23:55:26 +0000

    Add message schema and update register schema

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,10 +3,15 @@ import * as Yup from 'yup';
 export const registerSchema = Yup.object({
 	name: Yup.string().required('Name is required'),
 	email: Yup.string().email().required('Email is required'),
+	company: Yup.string().required('Company is required'),
 	password: Yup.string().min(8).required('Password is required'),
 });
 
 export const loginSchema = Yup.object({
 	email: Yup.string().email().required('Email is required'),
 	password: Yup.string().min(8).required('Password is required'),
+});
+
+export const messageSchema = Yup.object({
+	message: Yup.string().required('Message is required'),
 });
