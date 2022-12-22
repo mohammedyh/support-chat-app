145b7f3caa20081b855b8836ade3c13df34d5cf4
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-22 00:02:09 +0000

    Add types for socket backend

--- lib/types.ts
+++ lib/types.ts
@@ -9,3 +9,60 @@ export type NextApiResponseServerIO = NextApiResponse & {
 		};
 	};
 };
+
+export type ServerPacket =
+	| ContactsPacket
+	| MessagesPacket
+	| MessagePacket
+	| ShowFormPacket;
+
+export interface ContactsPacket {
+	type: 'contacts';
+	contacts: Contact[];
+}
+
+export interface MessagesPacket {
+	type: 'messages';
+	user: Contact | null;
+	messages: Message[];
+}
+
+export interface Contact {
+	id: string;
+	name: string;
+	email: string;
+	company: string;
+	status: string;
+	roles: string[];
+}
+
+export interface Message {
+	id: string;
+	from: string;
+	content: string;
+}
+
+export interface SendMessagePacket {
+	to: string;
+	content: string;
+	type: 'sendMessage';
+}
+
+export interface RequestUserPacket {
+	user: string;
+	type: 'requestUser';
+}
+
+export interface MessagePacket {
+	type: 'message';
+	id: string;
+	to: string | null;
+	from: string;
+	content: string;
+}
+
+export interface ShowFormPacket {
+	type: 'showForm';
+}
+
+export type ClientPacket = SendMessagePacket | RequestUserPacket;
