593e8505431fff2de63baf7fed6621851e41a569
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-22 00:08:02 +0000

    Make checks more robust and add default role

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -2,6 +2,7 @@ import * as bcrypt from 'bcryptjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { registerSchema } from '../../lib/auth';
 import dbConnect from '../../lib/dbConnect';
+import { getSession } from '../../lib/session';
 import validate from '../../lib/validate';
 import User from '../../models/User';
 
@@ -18,13 +19,28 @@ export default async function handler(
 	if (result) {
 		await dbConnect();
 
-		await User.create({
+		if (await User.findOne({ email: result.email.toLowerCase() })) {
+			return res.status(400).json({
+				success: false,
+				errors: [
+					{ message: 'User already exists with this email', path: 'email' },
+				],
+			});
+		}
+
+		const session = await getSession(req, res);
+
+		const user = await User.create({
 			name: result.name,
-			email: result.email,
+			email: result.email.toLowerCase(),
+			company: result.company,
 			password: await bcrypt.hash(result.password, 10),
+			roles: ['customer'],
 		});
 
-		return res.status(200).json({ redirect: '/login' });
+		session.userId = user._id.toHexString();
+
+		return res.status(200).json({ redirect: '/' });
 	}
 
 	return res.status(response.success ? 200 : 400).json(response);
