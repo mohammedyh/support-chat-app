4c8c1d80a7b411634b66d7c9440cf88d8c322692
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-22 00:20:04 +0000

    Add message model

---
+++ models/Message.ts
@@ -0,0 +1,25 @@
+import { addMinutes } from 'date-fns';
+import mongoose, { Document, Types } from 'mongoose';
+
+export interface IMessage extends Document {
+	from: Types.ObjectId;
+	to: Types.ObjectId | null;
+	content: string;
+	removedAt: Date;
+}
+
+const MessageSchema = new mongoose.Schema<IMessage>({
+	from: Types.ObjectId,
+	to: Types.ObjectId,
+	content: String,
+	removedAt: {
+		type: Date,
+		default: () => addMinutes(new Date(), 1),
+		expires: 0,
+	},
+});
+
+MessageSchema.set('timestamps', true);
+
+export default mongoose.models.Message ||
+	mongoose.model<IMessage>('Message', MessageSchema);
