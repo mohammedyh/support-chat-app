88e87d73fe70530a491bdcba0ee496d230ce7af3
mohammedyh <mohammedyousufhamid@gmail.com>
2022-12-22 00:08:23 +0000

    Remove comments

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -24,19 +24,16 @@ export default async function handler(
 		const user = await User.findOne({ email: result.email });
 
 		if (user) {
-			// Check if the inputted password matches the database hashed password
 			if (await bcrypt.compare(result.password, user.password)) {
 				session.userId = user._id.toHexString();
 				return res.status(200).json({ redirect: '/' });
 			} else {
-				// Valid email but invalid password
 				return res.status(401).json({
 					success: false,
 					errors: [{ path: 'password', message: 'Invalid password' }],
 				});
 			}
 		} else {
-			// Return HTTP 401 response and validation messages
 			return res.status(401).json({
 				success: false,
 				errors: [{ path: 'email', message: 'No user found with this email' }],
